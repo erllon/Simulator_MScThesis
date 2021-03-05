73f1ba5837297a57f267a9395be5215686a110ab
erllon <lone.erlend@gmail.com>
2021-03-05 10:21:29 +0100

    Added better animation capabilities

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
   )
 
 # %%Parameter initialization
-  _animate, save_animation = False, False
+  _animate, save_animation, plot_propterties = True, False, True
   start_animation_from_min_ID = 6
 
   max_range = 3 #0.51083#float(-np.log(-0.6))#3 #0.75    0.51083
@@ -188,8 +188,14 @@ if __name__ == "__main__":
   # F.plot_potential_field()
   # F.plot_force_field()
   #HER
-  fig, ax = plt.subplots(1,1)#plt.subplots(nrows=3,ncols=1)
-  ax.title.set_text("Deployment")# ax[0].title.set_text("Deployment")
+  if plot_propterties:
+    fig, ax = plt.subplots(nrows=3,ncols=1)
+    ax[0].title.set_text("Deployment")
+    ax[1].title.set_text(r"$\left\|\| F_{applied} \right\|\|$") #Set title
+    ax[2].title.set_text(r"$\xi$ from neighbors")               #Set title
+  else:
+    fig, ax = plt.subplots(1,1)#plt.subplots(nrows=3,ncols=1)
+    ax.title.set_text("Deployment")# ax[0].title.set_text("Deployment")
   #HER
   # ax[1].title.set_text(r"$\left\|\| F_{applied} \right\|\|$")
   # ax[2].title.set_text(r"$\xi$ from neighbors")
@@ -198,31 +204,33 @@ if __name__ == "__main__":
 
   if _animate: # TODO: if _animate: everything in same fig,  else: drones in one fig, "properties" in another fig
     for mn in mins[:start_animation_from_min_ID]:
-      mn.plot(ax)
-      # mn.plot(ax[0])
-      # mn.plot_traj_line(ax[0])
-      # mn.plot_force_traj_line(ax[1])
+      if plot_propterties:
+        mn.plot(ax[0])
+        mn.plot_traj_line(ax[0])
+        mn.plot_force_traj_line(ax[1])
+      else:
+        mn.plot(ax)
+
 
     offset, min_counter = [0], [start_animation_from_min_ID]
 
     def init():
-      if type(ax) == np.ndarray:
-        # scs.plot(ax[0])
-        # env.plot(ax[0])
-        scs.plot(ax)
-        env.plot(ax)
+      if plot_propterties:
+        scs.plot(ax[0])
+        env.plot(ax[0])
         artists = []
         for mn in mins:
-          artists += mn.plot(ax)
-          artists += (mn.plot_traj_line(ax), )
-          # artists += mn.plot(ax[0])
-          # artists += (mn.plot_traj_line(ax[0]), ) #Type: Line2D(_line6)
-          # artists += (mn.plot_force_traj_line(ax[1]), )
-          # artists += (mn.plot_xi_traj_line(ax[2]), )
+          # artists += mn.plot(ax)
+          # artists += (mn.plot_traj_line(ax), )
+          artists += mn.plot(ax[0])
+          artists += (mn.plot_traj_line(ax[0]), ) #Type: Line2D(_line6)
+          artists += (mn.plot_force_traj_line(ax[1]), )
+          artists += (mn.plot_xi_traj_line(ax[2]), )
           mn.plot_pos_from_pos_traj_index(0)
           mn.plot_force_from_traj_index(0)
           mn.plot_xi_from_traj_index(0)
-        ax[1].legend()
+        if start_animation_from_min_ID == 0:
+          ax[1].legend()  
       else:
         scs.plot(ax)
         env.plot(ax)
@@ -236,17 +244,46 @@ if __name__ == "__main__":
           # artists += (mn.plot_traj_line(ax[0]), )
           mn.plot_pos_from_pos_traj_index(0)
       return artists
+      # if type(ax) == np.ndarray:        
+      #   scs.plot(ax)
+      #   env.plot(ax)
+      #   artists = []
+      #   for mn in mins:
+      #     artists += mn.plot(ax)
+      #     artists += (mn.plot_traj_line(ax), )
+      #     # artists += mn.plot(ax[0])
+      #     # artists += (mn.plot_traj_line(ax[0]), ) #Type: Line2D(_line6)
+      #     # artists += (mn.plot_force_traj_line(ax[1]), )
+      #     # artists += (mn.plot_xi_traj_line(ax[2]), )
+      #     mn.plot_pos_from_pos_traj_index(0)
+      #     mn.plot_force_from_traj_index(0)
+      #     mn.plot_xi_from_traj_index(0)
+      #   ax[1].legend()
+      # else:
+      #   scs.plot(ax)
+      #   env.plot(ax)
+      #   # scs.plot(ax[0])
+      #   # env.plot(ax[0])
+      #   artists = []
+      #   for mn in mins:
+      #     artists += mn.plot(ax)
+      #     artists += (mn.plot_traj_line(ax), )
+      #     # artists += mn.plot(ax[0])
+      #     # artists += (mn.plot_traj_line(ax[0]), )
+      #     mn.plot_pos_from_pos_traj_index(0)
+      # return artists
 
     def animate(i):
       if i - offset[0] >= mins[min_counter[0]].get_pos_traj_length():
         offset[0] += mins[min_counter[0]].get_pos_traj_length()
         min_counter[0] += 1
       plt_pos_traj = mins[min_counter[0]].plot_pos_from_pos_traj_index(i - offset[0])
-      # plt_force_traj = mins[min_counter[0]].plot_force_from_traj_index(i-offset[0])
-      # plt_xi_traj = mins[min_counter[0]].plot_xi_from_traj_index(i-offset[0])
-      return plt_pos_traj, #plt_force_traj, plt_xi_traj  #mins[min_counter[0]].plot_pos_from_pos_traj_index(i - offset[0]), mins[min_counter[0]].plot_force_from_traj_index(i-offset[0]) #2
+      plt_force_traj = mins[min_counter[0]].plot_force_from_traj_index(i-offset[0])
+      plt_xi_traj = mins[min_counter[0]].plot_xi_from_traj_index(i-offset[0])
+      return plt_pos_traj, plt_force_traj, plt_xi_traj  #mins[min_counter[0]].plot_pos_from_pos_traj_index(i - offset[0]), mins[min_counter[0]].plot_force_from_traj_index(i-offset[0]) #2
 
     anim = FuncAnimation(fig, animate, init_func=init, interval=2, blit=False)
+    
     if save_animation:
       animation_name = "animation.gif"
       print("Saving animation")
@@ -254,27 +291,30 @@ if __name__ == "__main__":
       print(f"Animation saved to {animation_name}")
 
   else:
-    #HER
-    # env.plot(ax[0])
-    # scs.plot(ax[0])
-    env.plot(ax)
-    scs.plot(ax)
-    for mn in mins:
-      #HER
-      # mn.plot(ax[0])
-      # mn.plot_traj_line(ax[0])
-      mn.plot(ax)
-      mn.plot_traj_line(ax)
-      # mn.plot_force_traj_line(ax[1])
-      # mn.plot_xi_traj_line(ax[2])
-      # HER
+    if plot_propterties:
+      env.plot(ax[0])
+      scs.plot(ax[0])
+      for mn in mins:
+        mn.plot(ax[0])
+        mn.plot_traj_line(ax[0])
+
+      mn.plot_force_traj_line(ax[1])
+      mn.plot_xi_traj_line(ax[2])
       # ax[1].legend()
-      # ax.legend()
-
       #ax[2].legend()
-    ax.axis('equal')
+    else:
+      env.plot(ax)
+      scs.plot(ax)
+      for mn in mins:
+        mn.plot(ax)
+        mn.plot_traj_line(ax)
+      ax.legend()
+      #ax[2].legend()
+      ax.axis('equal')
 
-  
+  # if plot_propterties:
+  #   ax[1].legend()
+
   plt.show()
 
 
