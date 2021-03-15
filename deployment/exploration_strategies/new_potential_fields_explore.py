278c32550366715717d831263c2573b8b81b0dd0
erllon <lone.erlend@gmail.com>
2021-03-15 08:29:02 +0100

    Added rep force from other nodes

--- deployment/exploration_strategies/new_potential_fields_explore.py
+++ deployment/exploration_strategies/new_potential_fields_explore.py
@@ -13,9 +13,7 @@ import numpy as np
 
 class NewPotentialFieldsExplore(ExplorationStrategy):
     
-    def __init__(self, K_n=1, K_o=3
-    
-    , min_force_threshold=0.1):
+    def __init__(self, K_n=1, K_o=1, min_force_threshold=0.1):
         self.__K_n = K_n
         self.__K_o = K_o
         self.__min_force_threshold = min_force_threshold    
@@ -24,7 +22,7 @@ class NewPotentialFieldsExplore(ExplorationStrategy):
         MIN.compute_neighbors(beacons)
         RSSIs = [MIN.get_RSSI(n) for n in MIN.neighbors]
         F_att = NewPotentialFieldsExplore.get_attractive_force(self.__K_n, MIN)
-        F_rep = NewPotentialFieldsExplore.get_repulsive_force(self.__K_o, MIN, ENV)
+        F_rep = NewPotentialFieldsExplore.get_repulsive_force(self.__K_n, self.__K_o, MIN, ENV)
         F_sum = F_att + F_rep
         a = np.any([MIN.get_RSSI(n) for n in MIN.neighbors] >= self.MIN_RSSI_STRENGTH_BEFORE_LAND)
         b = [MIN.get_RSSI(n) for n in MIN.neighbors] >= self.MIN_RSSI_STRENGTH_BEFORE_LAND
@@ -34,14 +32,21 @@ class NewPotentialFieldsExplore(ExplorationStrategy):
 
     
     @staticmethod
-    def get_repulsive_force(K_n, MIN, ENV):
+    def get_repulsive_force(K_o, K_n, MIN, ENV):   
+        vecs_to_neighs = [
+            MIN.get_vec_to_other(n).reshape(2, 1) for n in MIN.neighbors if not (MIN.get_vec_to_other(n) == 0).all()
+        ]
+
         for s in MIN.sensors:
             s.sense(ENV)
         vecs_to_obs = [
             (R_z(MIN.heading)@R_z(s.host_relative_angle)@s.measurement.get_val())[:2]
             for s in MIN.sensors if s.measurement.is_valid()
         ]
-        return get_generic_force_vector(vecs_to_obs, K_n)
+
+        return get_generic_force_vector(vecs_to_neighs, K_n) + get_generic_force_vector(vecs_to_obs, K_o)
+
+    # return get_generic_force_vector(vecs_to_obs, K_n)
     
     @staticmethod
     def get_attractive_force(K_target, MIN):
