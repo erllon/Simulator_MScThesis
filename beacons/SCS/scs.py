75172410b059c89a4538e8d4e2e433a5031c63b2
erllon <lone.erlend@gmail.com>
2021-04-24 22:35:50 +0200

    Changed default values of init functions

--- beacons/SCS/scs.py
+++ beacons/SCS/scs.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 
 class SCS(Beacon):
 
-    def __init__(self, scs_id, range, xi_max=5, d_perf=1, d_none=3,  k=1, a=1, v=np.array([1, 0]), pos=None):
+    def __init__(self, scs_id, range, xi_max=1, d_perf=1, d_none=3,  k=1, a=1, v=np.array([1, 0]), pos=None):
         super().__init__(scs_id, range, xi_max, d_perf, d_none, k, a, v, pos)
         self.path_tree = PathTree(self)
     
