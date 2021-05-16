Add d_tau and correct scaling

beacons/MIN/min.py
@@ -46,8 +46,8 @@ class Min(Beacon):
     VectorTypes.INTERVAL: "orange"
   }
 
-  def __init__(self, min_id, max_range, deployment_strategy, xi_max=1, d_perf=1, d_none=3, k=0, a=0, v=np.zeros((2, ),), K_target=1, target_threshold=0.15, delta_expl_angle=np.pi/4):
-    super().__init__(min_id, max_range,xi_max, d_perf, d_none, pos=None)
+  def __init__(self, min_id, max_range, deployment_strategy, xi_max, d_perf, d_none, d_tau, k=0, a=0, v=np.zeros((2, ),), K_target=1, target_threshold=0.15, delta_expl_angle=np.pi/4):
+    super().__init__(min_id, max_range, xi_max, d_perf, d_none, d_tau, pos=None)
     self.K_target = K_target
     self.deployment_strategy = deployment_strategy
     self.target_threshold = target_threshold
@@ -179,7 +179,7 @@ class Min(Beacon):
       if not (MIN.get_vec_to_other(n) == 0).all():
         vec_from_neigh = -MIN.get_vec_to_other(n)#.reshape(2,))
         dist = np.linalg.norm(vec_from_neigh) #when using xi for RSSI, dist will be in the interval (0, 1.7916)
-        scaling = 4#2#4#2#4#MIN.d_none#1.7916#
+        scaling = MIN.d_tau#4#2#4#2#4#MIN.d_none#1.7916#
 
         vecs_from_neighs.append(((scaling-dist)/scaling)*MIN.range*normalize(vec_from_neigh))
         # vecs_from_neighs.append((MIN.range*(scaling-dist)/scaling)*normalize(vec_from_neigh))
@@ -208,7 +208,7 @@ class Min(Beacon):
         meas_length = np.linalg.norm(vec_from_obs)
         """Vector FROM drone TO obstacle"""
 
-        vec_from_obs = (MIN.range - meas_length)*MIN.range*normalize(vec_from_obs)
+        vec_from_obs = (MIN.range - meas_length)*normalize(vec_from_obs)
         vec_from_obs_scaled = (MIN.range - meas_length)*MIN.range*normalize(vec_away_from_obs)
         # vec_from_obs_scaled = ((MIN.range - meas_length)/1)*normalize(vec_away_from_obs)
 

beacons/SCS/scs.py
@@ -6,8 +6,8 @@ from copy import deepcopy
 
 class SCS(Beacon):
 
-    def __init__(self, scs_id, range, xi_max, d_perf, d_none, k=1, a=1, v=np.array([1, 0]), pos=None):
-        super().__init__(scs_id, range, xi_max, d_perf, d_none, k, a, v, pos)
+    def __init__(self, scs_id, range, xi_max, d_perf, d_none, d_tau, k=1, a=1, v=np.array([1, 0]), pos=None):
+        super().__init__(scs_id, range, xi_max, d_perf, d_none, d_tau, k, a, v, pos)
         self.path_tree = PathTree(self)
     
     def generate_target_pos(self, beacons, ENV, next_min):

beacons/beacon.py
@@ -22,7 +22,7 @@ class Beacon():
     Beacon.ID_counter += 1
     return ret
 
-  def __init__(self, b_id, range, xi_max, d_perf, d_none, k=0, a=0, v=np.zeros((2, )), pos=None):
+  def __init__(self, b_id, range, xi_max, d_perf, d_none, d_tau, k=0, a=0, v=np.zeros((2, )), pos=None):
     self.range = range
     self.target_r = range #TODO: Scale this down, so that the target is not generated at border?
     self.pos = pos
@@ -34,6 +34,7 @@ class Beacon():
     self.xi_max = xi_max
     self.d_perf = d_perf
     self.d_none = d_none
+    self.d_tau = d_tau
     self._omega = np.pi/(self.d_none - self.d_perf)
     self._phi = -(np.pi*self.d_perf) / (self.d_none - self.d_perf)
     self._xi_max_decrease = (self.xi_max/2)*self._omega